9a1f58b576f93481505d9711f8520f4367d54829
Ashish <jadhavashish028@gmail.com>
2023-04-21 02:36:37 +0530

    Update in Data_base.py file 21-04-2023

--- Data_base.py
+++ Data_base.py
@@ -1,10 +1,10 @@
 import pyodbc
-cnxn = pyodbc.connect("Driver={SQL Server};"
+con = pyodbc.connect("Driver={SQL Server};"
                       "Server=LAPTOP-K9QFULHI\SQLEXPRESS;"
                       "Database=RPA;"
                       "Trusted_Connection=yes;")
 
-cursor = cnxn.cursor()
+cursor = con.cursor()
 cursor.execute('select * from emp_details;')
 
 for row in cursor:
