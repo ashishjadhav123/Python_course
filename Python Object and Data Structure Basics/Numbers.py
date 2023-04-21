bba064d9750215066a38db61a48fdb1e8e904dde
Ashish <jadhavashish028@gmail.com>
2023-04-22 03:08:25 +0530

    Update in Numbers.py file 21-04-2023

---
+++ Python Object and Data Structure Basics/Numbers.py
@@ -0,0 +1,14 @@
+print(1 + 2)
+
+print(10/5)
+
+print(10*3)
+
+print('Hello my name is {}'.format('Ashish'))
+
+print('Hello my name is {0}, I am from {2}, I want to become {1}'.format('Ashish','India', 'Data Engineer'))
+
+result = 100/777
+
+print("Result is : {r:1.4f}".format(r=result))
+
